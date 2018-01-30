7ea33aba51caa0456951dd1944a95bd9b15b9f50
Juan D Giraldo M <judagima@gmail.com>
2018-01-30 09:55:43 -0500

    Clase Utilidades agregada

---
+++ AppCajero/Utilidades/Utilidades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCajero.Utilidades
+{
+    public static class Utilidades
+    {
+        public static List<T> Shuffle<T>(this List<T> original)
+        {
+            T[] shuffled = original.ToArray();
+
+            for (int index = shuffled.Length - 1; index > 0; index--)
+            {
+                int randomIndex = new Random().Next(index);
+
+                T temp = shuffled[index];
+                shuffled[index] = shuffled[randomIndex];
+                shuffled[randomIndex] = temp;
+            }
+
+            return shuffled.ToList();
+        }
+    }
+}
