e242021831512870c44d2044e1bdfb131aa38833
Juan D Giraldo M <judagima@gmail.com>
2018-01-30 21:53:45 -0500

    Utilidades actualizada
    Implementación de codificación Hash

--- AppCajero/Utilidades.cs
+++ AppCajero/Utilidades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,27 @@ namespace AppCajero
             }
 
             return shuffled.ToList();
+        }
+
+        public static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));            
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
+        {
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hashOfInput, hash))
+                return true;
+            else
+                return false;
         }
     }
 }
